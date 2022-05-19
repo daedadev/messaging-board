abc96ed79fbf22958d03863d69da990fda04cba0
Daniel Moore <wron2@hotmail.com>
2022-05-19 06:19:19 -0700

    Added firebase and vite env files

---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly env: {
+    VITE_API_KEY: string;
+    VITE_AUTH_DOMAIN: string;
+    VITE_PROJECT_ID: string;
+    VITE_STORAGE_BUCKET: string;
+    VITE_MESSAGING_SENDER_ID: string;
+    VITE_APP_ID: string;
+  };
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import db from "../config/firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 function App() {
   return (
